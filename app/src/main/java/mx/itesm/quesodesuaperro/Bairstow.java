ae7ffd9acfdd6d4d8731aaf6b81de3fac743aa57
Kaliar <julio.gon.men96@gmail.com>
2017-05-11 19:16:13 -0500

    Este commit se lo dedico a Cámara y Jona

--- app/src/main/java/mx/itesm/quesodesuaperro/Bairstow.java
+++ app/src/main/java/mx/itesm/quesodesuaperro/Bairstow.java
@@ -1,18 +1,128 @@
 package mx.itesm.quesodesuaperro;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Julio on 24/04/2017.
  */
 
 public class Bairstow extends android.support.v4.app.Fragment implements View.OnClickListener {
+
+    private Button calcular;
+    private EditText valores;
+    private TextView resultados;
+    private ArrayList<Double> coeficientes;
+    private TextView titulo;
+    private TextView instrucciones;
+    private TextView resTitle;
+
+
+
+
     public Bairstow(){
         //Constructor vacio requerido
     }
 
-    @Override
-    public void onClick(View v) {
-
+    public ArrayList<Double> divSinDoble(ArrayList<Double> coef, double r, double s){
+        ArrayList<Double> copia = new ArrayList<Double>(coef.size());
+        for(Double dab : coef){
+            copia.add(dab);
+        }
+        for(int i = 1; i < copia.size(); i++){
+            if(i<2) {
+                System.out.print("De " + copia.get(i));
+                copia.set(i, copia.get(i) + copia.get(i - 1) * r);
+                System.out.println(" A " + copia.get(i));
+            } else {
+                System.out.print("De " + copia.get(i));
+                copia.set(i,copia.get(i) + copia.get(i - 1) * r + copia.get(i - 2) * s);
+                System.out.println(" A " + copia.get(i));
+            }
+        }
+        return copia;
     }
+
+    public ArrayList<Double> bairSimple(ArrayList<Double> coef, double prec){
+        ArrayList<Double> b = coef, c, fin = new ArrayList<Double>(),copia = coef;
+        int j = 0;
+        double delta, r=-1, s=-1, deltaR, deltaS;
+        int lengthB = 0,lengthC = 0;
+        while(Math.sqrt(Math.abs(Math.pow(b.get(b.size()-1),2)+Math.pow(b.get(b.size()-2),2)))>prec){
+            System.out.println("coef " + coef.toString());
+            j++;
+            System.out.println("Condicion " + Math.abs(b.get(b.size()-1)*b.get(b.size()-2)));
+            b = divSinDoble(coef, r, s);
+            System.out.println("Hice b " + b.toString() +" con div y ahora c");
+            c = divSinDoble(b,r,s);
+            System.out.println("Hice c " + c.toString());
+            lengthC = c.size();
+            lengthB = b.size();
+            delta = Math.pow(c.get(lengthC-3),2)-c.get(lengthC-4)*c.get(lengthC-2);
+
+
+            deltaR = ((c.get(lengthC-4)*b.get(lengthB-1))-(c.get(lengthC-3)*b.get(lengthB-2)))/delta;
+
+            deltaS = ((b.get(lengthB-2)*c.get(lengthC-2))-(b.get(lengthB-1)*c.get(lengthC-3)))/delta;
+
+            r+=deltaR;
+            s+=deltaS;
+            System.out.println("delta " + delta + " r "+ r + " s " + s);
+        }
+        System.out.println("Condicion " + Math.sqrt(Math.abs(Math.pow(b.get(b.size()-1),2)+Math.pow(b.get(b.size()-2),2))));
+        for(int i = 0; i < lengthB-2; i++){
+            fin.add(b.get(i));
+        }
+
+        return fin;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_bairstow, container, false);
+        calcular = (Button) view.findViewById(R.id.calcular);
+        valores = (EditText) view.findViewById(R.id.coeficientes);
+        resultados = (TextView) view.findViewById(R.id.resultados);
+        resTitle = (TextView) view.findViewById(R.id.resTitle);
+        titulo = (TextView) view.findViewById(R.id.titulo);
+        instrucciones = (TextView) view.findViewById(R.id.instrucciones);
+        calcular.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view){
+        switch((view.getId())){
+            case R.id.calcular:
+                imprimirValor();
+                break;
+        }
+    }
+
+    private void imprimirValor() {
+        String res = "";
+        ArrayList<Double> prueba = new ArrayList<Double>();
+        prueba.add(6.0);
+        prueba.add(31.0);
+        prueba.add(63.0);
+        prueba.add(60.0);
+        prueba.add(44.0);
+        prueba.add(19.0);
+        prueba.add(-13.0);
+        prueba.add(-1.0);
+        prueba = bairSimple(prueba, 0.00000001);
+        System.out.println("Lo logré con " + prueba.size());
+        for(int i =0; i<prueba.size(); i++){
+            res+=prueba.get(i) + ", ";
+        }
+        resultados.setText(res);
+    }
+
 }
--- app/src/main/java/mx/itesm/quesodesuaperro/Gauss.java
+++ app/src/main/java/mx/itesm/quesodesuaperro/Gauss.java
@@ -1,7 +1,16 @@
 package mx.itesm.quesodesuaperro;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by Julio on 24/04/2017.
@@ -9,12 +18,198 @@ import android.view.View;
 
 public class Gauss extends Fragment implements View.OnClickListener {
 
+
+    private EditText linea;
+    private Button calcular;
+    private Button agregar;
+    private Button borrar;
+    private TextView valores;
+    private TextView resultados;
+    private Toast toast;
+    private int lineasFaltantes = -1;
+    private ArrayList<ArrayList<Double>> matriz;
+    private int longitud =0;
+    private double[][] matrizFinal;
+
     public Gauss(){
 
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.agregar:
+                try {
+                    leerEntrada(linea.getText().toString());
+
+                } catch(Exception e){
+                    toast = Toast.makeText(getActivity(), "Error en los valores ingresados" , Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                imprimirValores();
+                break;
+            case R.id.calcular:
+                if(lineasFaltantes>0){
+                    toast = Toast.makeText(getActivity(), "Faltan  agregar más líneas", Toast.LENGTH_LONG);
+                    toast.show();
+                } else if(lineasFaltantes== -1){
+                    toast = Toast.makeText(getActivity(), "Agrega líneas", Toast.LENGTH_LONG);
+                    toast.show();
+                 } else{
+                    double[][] matrizUltimate =traducir(matriz);
+                    System.out.println();
+                    imprimirResultados(matrizUltimate);
+                }
+
+                break;
+            case R.id.borrar:
+                eliminarValores();
+                imprimirValores();
+                break;
+        }
+    }
+
+    private void imprimirResultados(double[][] matrizFinal) {
+        String res = "";
+        for(int i = 0; i<matrizFinal.length; i++){
+            for(int j =0; j<matrizFinal[0].length; j++){
+                res+=matrizFinal[i][j] + ",";
+            }
+            res+="\n";
+        }
+        resultados.setText(res);
+    }
+
+    private double[][] traducir(ArrayList<ArrayList<Double>> matriz) {
+        matrizFinal = new double[matriz.size()][matriz.get(0).size()];
+        for(int i =0; i<matriz.size();i++){
+            for(int j =0; j<matriz.get(0).size(); j++){
+                matrizFinal[i][j] = matriz.get(i).get(j);
+                System.out.println(matrizFinal[i][j]);
+            }
+        }
+
+        matrizFinal = gauss(matrizFinal);
+        System.out.println("El resultado final es");
+        for(int i =0; i<matriz.size();i++){
+            for(int j =0; j<matriz.get(0).size(); j++){
+                System.out.println(matrizFinal[i][j]);
+            }
+        }
+        return matrizFinal;
+    }
+
+    private void eliminarValores() {
+        if(matriz.size()>1){
+            matriz.remove(matriz.size()-1);
+            toast = Toast.makeText(getActivity(), "Valores eliminados", Toast.LENGTH_LONG);
+            toast.show();
+            lineasFaltantes--;
+        } else if(matriz.size()==1){
+            matriz.remove(matriz.size()-1);
+            toast = Toast.makeText(getActivity(), "Valores eliminados", Toast.LENGTH_LONG);
+            toast.show();
+            lineasFaltantes = -1;
+        }
+    }
+
+    private void leerEntrada(String linea) throws Exception{
+        String[] valoresSimples = linea.split(",");
+        ArrayList<Double> coef = new ArrayList<Double>(valoresSimples.length);
+        for(int i = 0; i < valoresSimples.length; i++){
+            coef.add(Double.parseDouble(valoresSimples[i]));
+        }
+
+        if(lineasFaltantes == -1){
+            lineasFaltantes = coef.size()-2;
+            toast = Toast.makeText(getActivity(), "Líneas faltantes " + lineasFaltantes , Toast.LENGTH_LONG);
+            toast.show();
+            matriz.add(coef);
+            longitud = matriz.get(0).size();
+        } else if(coef.size() != longitud){
+            toast = Toast.makeText(getActivity(), "Longitud errónea", Toast.LENGTH_LONG);
+            toast.show();
+        }else if(lineasFaltantes<=0){
+            toast = Toast.makeText(getActivity(), "Número de lineas máximo alcanzado", Toast.LENGTH_LONG);
+            toast.show();
+        }else{
+            matriz.add(coef);
+            lineasFaltantes--;
+        }
 
     }
+    private void imprimirValores(){
+        String res = "";
+        for(ArrayList<Double> lista: matriz){
+            for(Double val: lista){
+                res+= val + ", ";
+            }
+            res+= "\n";
+        }
+        valores.setText(res);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_gauss, container, false);
+        matriz = new ArrayList<ArrayList<Double>>();
+        agregar = (Button) view.findViewById(R.id.agregar);
+        calcular = (Button) view.findViewById(R.id.calcular);
+        valores = (TextView) view.findViewById(R.id.valores);
+        resultados = (TextView) view.findViewById(R.id.Resultados);
+        linea = (EditText) view.findViewById(R.id.matriz);
+        agregar.setOnClickListener(this);
+        calcular.setOnClickListener(this);
+        borrar = (Button) view.findViewById(R.id.borrar);
+        borrar.setOnClickListener(this);
+
+        return view;
+    }
+
+
+    public double[][] gauss(double[][] matrix){
+        double pivote=1;
+        double numBajoPivote;
+        for(int fila=0;fila<matrix.length-1;fila++){
+            System.out.println("Me meti a gauss");
+            for(int filaSig=fila+1;filaSig<matrix.length;filaSig++){
+                if(matrix[fila][fila]==0){
+                    matrix=cambioDe0(matrix,fila);
+                }
+
+                for(int num=0;num<matrix[fila].length;num++){
+                    if(num==fila){
+                        pivote=matrix[fila][fila];
+                    }
+
+                    matrix[fila][num]=matrix[fila][num]/pivote;
+                }
+                numBajoPivote= matrix[filaSig][fila];
+                for(int num=0;num<matrix[filaSig].length;num++){
+                    matrix[filaSig][num]=(matrix[fila][num]*(-numBajoPivote))+matrix[filaSig][num];
+                }
+                pivote= matrix[filaSig][filaSig];
+                for(int k=0;k<matrix[filaSig].length;k++){
+                    matrix[filaSig][k]=(matrix[filaSig][k])/pivote;
+                }
+            }
+        }
+        return matrix;
+    }
+
+    private double[][] cambioDe0(double[][]matrix, int fila){
+        double[] temp;
+        while(matrix[fila][fila]==0){
+            for(int cambio=fila+1;cambio<matrix.length;cambio++){
+                if(matrix[cambio][fila]!=0){
+                    temp=matrix[fila];
+                    matrix[fila]=matrix[cambio];
+                    matrix[cambio]=temp;
+                }
+            }
+        }
+        return matrix;
+    }
+
 }
